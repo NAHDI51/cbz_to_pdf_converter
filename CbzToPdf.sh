f3927efaa1a32419b642a7207a41705b4d0501cb
NAHDI51 <hossainnahdi8051@gmail.com>
2023-01-04 16:59:20 +0600

    Initial commit

---
+++ CbzToPdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This program converts the comics/mangas into a Kindle 
+# supported format.
+
+# This program must be run as a root. So obviously, 
+
+# Of course, we can take 
+
+DEFAULT="${PWD}"
+
+"$1"
+
+DEST=
