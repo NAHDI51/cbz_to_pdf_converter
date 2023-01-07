20ec874c83e8e4d1707cf353e184eaa65188fcf7
NAHDI51 <hossainnahdi8051@gmail.com>
2023-01-07 16:16:36 +0600

    Exported and finished

---
+++ src/colors/colors.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# This file handles the colorization of the console (stdin, stdout)
+
+
+# Check if this file has previously been defined
+# If not, define it, so that other files can include it.
+
+if [$(echo -e $__COLOR_SH) -eq ""]; then
+    __COLOR_SH="1"
+fi
+
+#beginning and ending of a color prompt
+BEGCOL="\e["
+ENDCOL="\e[0m"
+
+# D=DARK
+# L=LIGHT
+
+BG_MODE=1
+FG_MODE=0
+
+# (Foreground codes, Background codes)
+BLACK=("30" "40")
+RED=("31" "41")
+GREEN=("32" "42")
+YELLOW=("33" "43")
+BLUE=("34" "44")
+MAGNETA=("35" "45")
+CYAN=("36" "46")
+LGRAY=("37" "47")
+GRAY=("90" "100")
+LRED=("91" "101")
+LGREEN=("92" "102")
+LYELLOW=("93" "103")
+LBLUE=("94" "104")
+LMAGNETA=("95" "105")
+LCYAN=("96" "106")
+WHITE=("97" "107")
+
+
+# formats
+
+REGULAR="0"
+BOLD="1"
+FAINT="2"
+ITALICS="3"
+UNDERLINED="4"
+BLINKING="5"
+
+# Debugging purposes
+
+FLAG="Reached here."
---
+++ src/colors/prompt.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# A generalized wrapper for colorize_output. Outputs messages
+# according to their indication (e.g info, error, warning). See
+# helpmsg for more details
+
+# define header: _COLORIZE_OUTPUT_SH
+if [[ $(echo -en $__PROMPT_SH) -eq "" ]]; then
+__PROMPT_SH="1"
+fi
+
+# dependency: include.sh
+. $(dirname "$0")/src/include.sh
+
+# Dependencies
+
+if [[ $(echo -en $__COLORIZE_OUTPUT_SH) -eq "" ]]; then
+include 'src/colors/colorize_output.sh'
+fi
+
+
+prompt() {
+    local helpmsg=("
+        Usage: prompt [MODE] [STATEMENT]\n
+        \n
+        This is a generalized wrapper for colorize_output. Output specific \n
+        indicative messages (e.g info, error, warning). \n
+        \n
+        This program assumes the first argument to be the mode, and the second \n
+        argument to be the statement. \n
+        \n
+        MODES \n
+        -h | --help  \t\t Displays this message. \n
+        -e | --error \t\t Indicates that the following message is an ERROR prompt. \n
+        -i | --info  \t\t Indicates that the following message is an INFO prompt. \n
+        -w | --warning \t Indicates that the following message is a WARNING prompt. \n
+        -s | --success \t Indicates that the following message is a SUCCESS prompt. \n    
+        -c | --caution \t Indicates that the following message is a CATUIONARY prompt. \n
+        \n
+        DEFAULTS \n
+        1. Error prompt prints the prompt in BOLD RED. \n
+        2. Info prompt prints the prompt in LIGHT GRAY. \n
+        3. Warning prompt prints the prompt in yellow. \n 
+        4. Success prompt prints the prompt in GREEN. \n
+        5. Default prompt prints the prompt in WHITE. \n
+        6. Caution prompt blinks the background in RED, while prints the prompt \n
+           in BLACK. \n
+        \n
+        For the time being, no argument-through changing of the defaults \n
+        is implemented. Thus, if you want to change, do it through the color \n
+        flags being called in below. \n
+        \n
+    ")
+    case $1 in 
+        "-h" | "--help")
+            echo -e "$helpmsg" ;;
+        "-e" | "--error")
+            colorize_output -r -F --red "$2" ;;
+        "-i" | "--info")
+            colorize_output -r -F --light-gray "$2" ;;
+        "-w" | "--warning")
+            # echo -e "$1 $2"
+            colorize_output -b -F --yellow "$2" ;;
+        "-s" | "--success")
+            colorize_output -b -F --light-green "$2" ;;
+        "-c" | "--caution")
+            colorize_output -l -B --light-red -F --black "$2" ;;
+        *)
+            echo -ne $1 ;;
+    esac
+
+    return 0
+}
