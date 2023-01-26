0afb9b67c9eac72cbbf7a77b7b8134bab42694fa
NAHDI51 <hossainnahdi8051@gmail.com>
2023-01-27 04:02:17 +0600

    Seperated root permission as its own source code

--- CbzToPdf.sh
+++ CbzToPdf.sh
@@ -3,39 +3,30 @@
 # This program converts the comics/mangas into a Kindle 
 # supported format (pdf)
 
-
 # Include C style dependency inclusion
 . $(dirname "$0")/src/include.sh
 
-# Equivalent to #ifndef
+# Dependency inclusions
+
 if [[ $__PROMPT_SH -eq "" ]]; then
 include 'src/colors/prompt.sh'
 fi
 
+if [[ $__RUN_AS_ROOT -eq "" ]]; then
+include 'src/run_as_root.sh'
+fi
+
+# Source header definition
+if [[ $__CBZTOPDF -eq "" ]]; then
+__CBZTOPDF=1
+fi
+
+
 # This program must be run as a root. So obviously, we need to
 # check whether the current user is root. If not, let's ask for the 
-# password. 
+# password.
 
-if [[ "$USER" != "root" ]]; then
-    prompt -w "WARNING: This program has to be run by root.\n"
-    prompt -i "Do you want to run this program as a root? [y/n]: "
-    read RESPONSE
+run_as_root
 
-    case $RESPONSE in 
-        "y" | "Y" | "yes" | "Yes") 
-            prompt -i "Entering the program as root.\n"
-            sudo ./CbzToPdf.sh
-            exit 0
-            ;;
-        "n" | "N" | "no" | "No")
-            prompt -s "Exitting the program.\n"
-            exit 0
-            ;;
-        *)
-            prompt -e "FATAL ERROR: Unknown answer. Exitting the program. \n"
-            exit 1
-            ;;
-    esac
-# Of course, we can take 
 
 DEFAULT="${PWD}"
